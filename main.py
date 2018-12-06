78f8dd5c5d8cb97c7fe17a60187fce65afbe2575
Gauthier Heiss <gauthier.heiss@gmail.com>
2018-12-06 23:05:21 +0100

    Adding random comments, because it's fun you know

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+# import various things for random purposes
 from commands import commands
 import functions
 import random
 
-
+# You know you're fine when your chatbot is written in 5 lines
 def compute(msg):
+	#
 	for cmd in commands:
+		# Wow, such one line python
 		if any(word.lower() in msg.lower() for word in cmd['catchers']):
 			if "command" in cmd:
+				# Can certainly cause a security issue, but who care, it's just a bot
 				return getattr(functions, cmd['command'])(msg)
 			else:
 				return random.choice(cmd['answers'])
