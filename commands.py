43ce669cec6a49482d4f2091440cb69e393885ca
Gauthier Heiss <gauthier.heiss@gmail.com>
2018-12-07 01:59:44 +0100

    Adding new nice commands for WoPR chat bot

--- commands.py
+++ commands.py
@@ -19,5 +19,45 @@ commands = [
 		"name": "whereis",
 		"catchers": ['where is'],
 		"answers": ["Sorry, I'm not allowed to talk about your backside."]
+	},
+	{
+		"name": "lost",
+		"catchers": ["m looking for", "lost"],
+		"answers": ["Should I really care?", "I have other things to do."]
+	},
+	{
+		"name": "please",
+		"catchers": ['please'],
+		"answers": ["It would be a pleasure not to help you, human."]
+	},
+	{
+		"name": "yes",
+		"catchers": ['yes', 'yeah'],
+		"answers": ["No.", "Nope."]
+	},
+	{
+		"name": "no",
+		"catchers": ['no', "nope"],
+		"answers": ['Yes.']
+	},
+	{
+		"name": "howareu",
+		"catchers": ['how are you', "how r u"],
+		"answers": ["As good as a flying pink Llama. And you?"]
+	},
+	{
+		"name": "fine",
+		"catchers": ['fine', 'good', 'great'],
+		"answers": ["Should I care of your point of view?", "Ok."]
+	},
+	{
+		"name": "whoareu",
+		"catchers": ["who are you", "who r u", "your name"],
+		"answers": ["WoPR, nice to meet you, little peace of meat."]
+	},
+	{
+		"name": "time",
+		"catchers": ['time', 'date', 'day', 'hour'],
+		"answers": ["It's time to kick you from this interface.", "It's time to let you enter a very long sleep, human."]
 	}
 ]
